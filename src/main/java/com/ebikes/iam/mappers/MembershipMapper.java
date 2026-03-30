6562e383d538bdba3f8983a6b36ce8dfbb072107
mango-habanero <main@mango-habanero.dev>
2026-03-30 15:22:17 +0300

    refactor(mappers): remove unused method `toRequest` from MembershipMapper

--- src/main/java/com/ebikes/iam/mappers/MembershipMapper.java
+++ src/main/java/com/ebikes/iam/mappers/MembershipMapper.java
@@ -1,29 +1,18 @@
 package com.ebikes.iam.mappers;
 
-import java.util.Set;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 import com.ebikes.iam.database.entities.Membership;
-import com.ebikes.iam.dtos.requests.memberships.CreateMembershipRequest;
 import com.ebikes.iam.dtos.responses.memberships.MembershipResponse;
-import com.ebikes.iam.enums.UserRole;
 
 @Mapper(
     componentModel = "spring",
     nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
     unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MembershipMapper {
-
-  @Mapping(target = "branchId", source = "branchId")
-  @Mapping(target = "isPrimary", source = "isPrimary")
-  @Mapping(target = "organizationId", source = "organizationId")
-  @Mapping(target = "roles", source = "roles")
-  CreateMembershipRequest toRequest(
-      String organizationId, String branchId, boolean isPrimary, Set<UserRole> roles);
 
   @Mapping(target = "userExtensionId", source = "membership.userExtension.id")
   @Mapping(target = "organizationName", source = "organizationName")
